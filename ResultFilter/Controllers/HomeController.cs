5ecd076a8a103e510c397fa9d2abaae9e261d00b
Amit <amitkr24031995@gmail.com>
2022-04-22 17:01:07 +0530

    Adding ResultFilter

---
+++ ResultFilter/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ResultFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResultFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        PersonDBEntities db = new PersonDBEntities();
+        // GET: Home
+        [OutputCache(Duration =10, Location =System.Web.UI.OutputCacheLocation.Any)]
+        public ActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            var data = db.people.ToList();
+            return View(data);
+        }
+    }
+}
